334dd1fcae1d5e5e423c062a1d3066004a10d06a
abnormallynormal <yichenxiao08@gmail.com>
2025-11-19 13:51:31 -0500

    points done

--- src/app/points/page.tsx
+++ src/app/points/page.tsx
@@ -12,8 +12,13 @@ import {
   PopoverContent,
 } from "@/components/ui/popover";
 import { Filter } from "lucide-react";
-
-export default function Finances() {
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+export default function Points() {
   const [data, setData] = useState<PlayerWithPoints[]>();
   const [pointsFilter, setPointsFilter] = useState<{
     lower: number | undefined;
@@ -66,60 +71,131 @@ export default function Finances() {
     <>
       <Navigation />
       <div className="px-16 py-8">
-        <div className="max-w-4xl justify-self-center w-full">
+        <div className="justify-self-center w-full">
           <div className="mb-2 justify-between">
-            <div className="font-bold text-3xl mb-4">Points</div>
-            <div className="flex items-center gap-4">
-              <Input
-                placeholder="Filter by name"
-                value={filter}
-                onChange={(e) => setFilter(e.target.value)}
-              />
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="secondary" size="icon">
-                    <Filter />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-72 mr-4">
-                  <div className="flex flex-col gap-2">
-                    <div className="font-semibold">Filter by points</div>
-                    <Input
-                      placeholder="Lower bound"
-                      type="number"
-                      value={pointsFilter?.lower}
-                      onChange={(e) =>
-                        setPointsFilter((prev) => ({
-                          lower: Number(e.target.value),
-                          upper: prev?.upper,
-                        }))
-                      }
-                    />
-                    <Input
-                      placeholder="Upper bound"
-                      type="number"
-                      value={pointsFilter?.upper}
-                      onChange={(e) =>
-                        setPointsFilter((prev) => ({
-                          lower: prev?.lower,
-                          upper: Number(e.target.value),
-                        }))
-                      }
-                    />
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      onClick={() => setPointsFilter(undefined)}
-                    >
-                      Clear Filters
-                    </Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
+            <div className="font-bold text-4xl mb-2">
+              This year's recipients
             </div>
+            <Accordion type="multiple" className="w-full mb-8">
+              <AccordionItem value="outstanding-contribution">
+                <AccordionTrigger>
+                  <div>
+                    <div className="font-semibold text-2xl hover:underline mb-1">
+                      Outstanding Contribution
+                    </div>
+                    <div className="text-lg">Gr. 12: 100+ points</div>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {data
+                    ?.filter((student) => student.points >= 100)
+                    .map((student) => (
+                      <div
+                        key={student.student_id}
+                        className="text-sm text-gray-500"
+                      >
+                        {student.name}: {student.points} points
+                      </div>
+                    ))}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="letter-of-distinction">
+                <AccordionTrigger>
+                  <div>
+                    <div className="font-semibold text-2xl hover:underline mb-1">
+                      Letter of Distinction
+                    </div>
+                    <div className="text-lg">90 points</div>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {data
+                    ?.filter((student) => student.points >= 90)
+                    .map((student) => (
+                      <div
+                        key={student.student_id}
+                        className="text-sm text-gray-500"
+                      >
+                        {student.name}: {student.points} points
+                      </div>
+                    ))}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="letter-of-merit">
+                <AccordionTrigger>
+                  <div>
+                    <div className="font-semibold text-2xl hover:underline mb-1">
+                      Letter of Merit
+                    </div>
+                    <div className="text-lg">60 points</div>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {data
+                    ?.filter((student) => student.points >= 60)
+                    .map((student) => (
+                      <div
+                        key={student.student_id}
+                        className="text-sm text-gray-500"
+                      >
+                        {student.name}: {student.points} points
+                      </div>
+                    ))}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
-          <DataTable columns={columns} data={filteredData ?? []} />
+          <div className="font-bold text-3xl mb-4">Points</div>
+          <div className="flex items-center gap-4 mb-2">
+            <Input
+              placeholder="Filter by name"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            />
+            {/* <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="secondary" size="icon">
+                  <Filter />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-72 mr-4">
+                <div className="flex flex-col gap-2">
+                  <div className="font-semibold mb-2">Filter by points</div>
+                  <Input
+                    placeholder="Lower bound"
+                    type="number"
+                    value={pointsFilter?.lower}
+                    onChange={(e) =>
+                      setPointsFilter((prev) => ({
+                        lower: Number(e.target.value),
+                        upper: prev?.upper,
+                      }))
+                    }
+                  />
+                  <Input
+                    placeholder="Upper bound"
+                    type="number"
+                    value={pointsFilter?.upper}
+                    onChange={(e) =>
+                      setPointsFilter((prev) => ({
+                        lower: prev?.lower,
+                        upper: Number(e.target.value),
+                      }))
+                    }
+                  />
+                  <Button
+                    variant="secondary"
+                    size="sm"
+                    onClick={() => setPointsFilter(undefined)}
+                  >
+                    Clear Filters
+                  </Button>
+                </div>
+              </PopoverContent>
+            </Popover> */}
+          </div>
         </div>
+        <DataTable columns={columns} data={filteredData ?? []} />
       </div>
     </>
   );
